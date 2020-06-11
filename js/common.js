613b7d997c6d07a52adb46233fad90283527fa89
mao <maoyuyin@126.com>
2020-06-11 20:15:12 +0800

    OK

---
+++ js/common.js
@@ -0,0 +1,34 @@
+
+var result = /(ipod|ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i.test(navigator.userAgent);
+
+if(!result){
+  location.href = "https://axcd.github.io/web/403";
+};
+
+function load(){
+
+  document.body.style.height = (screen.availHeight-50) + "px";
+  
+}
+
+function loadLayout(){
+ 
+  var rows = document.getElementsByClassName("row");
+  var rowX = screen.availWidth-10;
+  var rowY = (screen.availWidth-10)/2+26;
+
+  for (i = 0; i < rows.lenth; i++) {
+    rows[i].style.width = rowX + "px";
+    rows[i].style.height = rowY + "px";
+  }
+
+  var imgs = document.getElementsByTagName("img");
+  var imgX = (screen.availWidth-10)/2-20;
+  var imgY = (screen.availWidth-10)/2-20;
+
+  for (i = 0; i < imgs.length; i++) {
+    imgs[i].style.width = imgX + "px";
+    imgs[i].style.height = imgY + "px";
+  }
+
+}
